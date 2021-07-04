54c3b10710289f9abe11384916de5f6cf76ead98
DemonVetarian <klintonpcb@gmail.com>
2021-07-04 23:24:27 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ComponentFactoryResolver } from '@angular/core';
+import { HostDirective} from './host.directive';
+import { HelloComponent } from './hello/hello.component';
+import { HiComponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Oreo';
-}
+  display=false;
+  
+  constructor(public factoryRes: ComponentFactoryResolver){
+    
+  }
+  
+  /*@ViewChild(HostDirective, { static: true })
+  childRef!: HostDirective;
+  
+  components = [HiComponent, HelloComponent];
+
+
+  show(id: any)
+  {
+    this.childRef.viewRef.clear();
+    //Selecting the component from array
+    const resolvedFactory = this.factoryRes.resolveComponentFactory(
+      this.components[id]
+    );
+    //Dumping the content inside the tag
+    const compRef = this.childRef.viewRef.createComponent(resolvedFactory);
+    //Giving the @input to child
+    compRef.instance.name = id == 0 ? 'Gopi' : 'Surendran';
+    console.log(compRef);
+  }*/
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { LeftComponent } from './left/left.component';
+import { HelloComponent } from './hello/hello.component';
+import { HiComponent } from './hello/hello.component';
+import { BindigsComponent } from './bindigs/bindigs.component';
+import { ExampleDirective } from './example.directive';
+import { StructureDirective } from './structure.directive';
+import { StucAttrComponent } from './stuc-attr/stuc-attr.component';
+import { ObserComponent } from './obser/obser.component';
+import { CrudtestComponent } from './crudtest/crudtest.component';
+import { CtComponent } from './ct/ct.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Crudservice1Service } from './crudservice1.service';
+import { MycrudComponent } from './mycrud/mycrud.component';
+import { ServicecrudService } from './servicecrud.service';
+import { HttpintercepterComponent } from './httpintercepter/httpintercepter.component';
+import { RoutingappComponent } from './routingapp/routingapp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    LeftComponent,
+    HelloComponent,
+    HiComponent,
+    BindigsComponent,
+   ExampleDirective,
+    StructureDirective,
+    StucAttrComponent,
+    ObserComponent,
+    CrudtestComponent,
+    CtComponent,
+    MycrudComponent,
+    HttpintercepterComponent,
+    RoutingappComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
-  providers: [],
+  entryComponents: [HelloComponent, HiComponent],
+  providers: [ServicecrudService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
